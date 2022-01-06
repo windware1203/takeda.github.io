d9fe4c489930b393413e1383322fad80fe0c3e94
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2022-01-06 23:30:34 +0800

    feat. learning

---
+++ learn.js
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+function draw() {
+  var canvas = document.getElementById('cyoShow');
+  if (canvas.getContext){
+    var ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.moveTo(75,50);
+    ctx.lineTo(80,100);
+    ctx.lineTo(70,100);
+    ctx.fill();
+  }
+}
+=======
+function draw() {
+  var canvas = document.getElementById('cyoShow');
+  if (canvas.getContext){
+    var ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.moveTo(75,50);
+    ctx.lineTo(80,100);
+    ctx.lineTo(70,100);
+    ctx.fill();
+  }
+}
+>>>>>>> aacbcfb645019813365d6625bfb9d89b67d0ec3b
