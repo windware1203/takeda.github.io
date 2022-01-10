8ec2152a2a4b5192e2f2bdd83357017f14b97ec2
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2022-01-10 22:24:18 +0800

    feat: easy cipher algorithm

--- learn.js
+++ learn.js
@@ -1,4 +1,24 @@
+function getRandomInt(max) 
+{
+    
+    return Math.floor(Math.random() * max);
+}
 function rot13()
 {
     document.getElementById("rot13_output").value = document.getElementById("rot13_input").value.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+}
+function cipher()
+{
+    let the_key = (getRandomInt(Date.parse('2020-10-10'))%69)+1;
+    document.getElementById("key").innerHTML = the_key;
+    let the_string = document.getElementById("cipher_input").value
+    let len = the_string.length;
+    let the_cipher = "";
+    let the_plain = "";
+    for(var i = 0 ; i<len ;i++)
+    {
+        the_cipher += String.fromCharCode(the_string.charCodeAt(i) ^ the_key);
+    }
+    
+    document.getElementById("cipher_output").value = the_cipher;
 }
