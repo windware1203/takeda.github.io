01ccaf916b0b271d6531006bd6616118763961ff
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2022-01-11 00:43:50 +0800

    feat: test and sim.

--- game.js
+++ game.js
@@ -1,4 +1,64 @@
 var name = "";
+var pionts = 0;
+var charArr = [];
+
+
+function init()
+{
+    rePoint();
+    fullCharArr();
+    genRot13();
+}
+function fullCharArr()
+{
+    for(var i = 0 ; i<26 ;i++)
+    {
+        charArr[i] = String.fromCharCode(i+65);
+        
+    }
+    
+    for(var i = 26 ; i<52 ;i++)
+    {
+        charArr[i] = String.fromCharCode(i+71);
+        
+    }
+
+}
+
+function getRandomInt(max) 
+{
+    
+    return Math.floor(Math.random() * max);
+}
+
+var rot13_q = "";
+function genRot13()
+{
+    for(var i = 0 ; i<26 ;i++)
+    {
+        let x = getRandomInt(50);
+        let y = getRandomInt(50);
+        let temp = 0;
+        
+        temp = charArr[x];
+        charArr[x] = charArr[y];
+        charArr[y] = temp;
+ 
+    }
+    
+    for(var i = 0 ; i<5 ;i++)
+        rot13_q += charArr[i];
+
+
+    document.getElementById("rot13_str").innerHTML = rot13_q;
+}
+
+function rot13()
+{
+    let ref = document.getElementById("fb1");
+    (document.getElementById("rot13_output").value == rot13_q.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);})) ? (ref.innerHTML="O") : (ref.innerHTML="X") ;
+}
+
 function submit()
 {
     var nameInput = document.getElementById("name");
@@ -14,4 +74,9 @@ function submit()
         document.getElementById("nameHTML").innerHTML = "Hi, " + name;
     }
     
+}
+
+function rePoint()
+{
+    document.getElementById("score").innerHTML = pionts;
 }
--- learn.js
+++ learn.js
@@ -7,9 +7,10 @@ function rot13()
 {
     document.getElementById("rot13_output").value = document.getElementById("rot13_input").value.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
 }
+
 function cipher()
 {
-    let the_key = (getRandomInt(Date.parse('2020-10-10'))%69)+1;
+    let the_key = getRandomInt(98)+1;
     document.getElementById("key").innerHTML = the_key;
     let the_string = document.getElementById("cipher_input").value
     let len = the_string.length;
