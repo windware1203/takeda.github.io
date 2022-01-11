441c5c7b5950cc3a767d178e6181e4dfbd52368a
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2022-01-11 23:55:55 +0800

    feat: pic

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
 var name = "";
 var pionts = 0;
 var charArr = [];
-
+var rankImg = ["img/0.png", "img/1.png", "img/2.png", "img/3.png", "img/4.png",
+               "img/5.png", "img/6.png", "img/7.png", "img/8.png", "img/9.png",
+               "img/10.png", "img/11.png", "img/12.png", "img/13.png", "img/14.png",
+               "img/15.png", "img/16.png", "img/17.png", "img/18.png", "img/19.png"];
+var rank = 0;
 
 function init()
 {
@@ -52,11 +56,30 @@ function genRot13()
 
     document.getElementById("rot13_str").innerHTML = rot13_q;
 }
-
+function q2()
+{
+    let ref = document.getElementById("fb2");
+    let button = document.getElementById("b2");
+    (document.getElementById("q2_flag").value == "nanaCTF{dHJddgdf_j;d;;gdgfd}") ? (ref.innerHTML="O") : (ref.innerHTML="X") ;
+    if((ref.innerHTML=="O"))
+    {
+        button.style.display = "none";
+        pionts += 10;
+        rePoint();
+    }
+        
+}
 function rot13()
 {
     let ref = document.getElementById("fb1");
+    let button = document.getElementById("b1");
     (document.getElementById("rot13_output").value == rot13_q.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);})) ? (ref.innerHTML="O") : (ref.innerHTML="X") ;
+    if((ref.innerHTML=="O"))
+    {
+        button.style.display = "none";
+        pionts += 10;
+        rePoint();
+    }
 }
 
 function submit()
@@ -79,4 +102,9 @@ function submit()
 function rePoint()
 {
     document.getElementById("score").innerHTML = pionts;
+    rank = Math.floor(pionts/10);
+    console.log(rank);
+    document.getElementById("rank").innerHTML = rank;
+
+    document.getElementById("rankImg").src = rankImg[rank];
 }
