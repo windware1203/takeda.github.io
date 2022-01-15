0362895e3622a733a94285e991441244783581b0
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2022-01-15 23:36:15 +0800

    feat and style

--- learn.js
+++ learn.js
@@ -8,6 +8,7 @@ function rot13()
     document.getElementById("rot13_output").value = document.getElementById("rot13_input").value.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
 }
 function setup() {
+  
   var e = document.getElementById("watchme");
   e.addEventListener("animationstart", listener, false);
   e.addEventListener("animationend", listener, false);
