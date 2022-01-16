c3100462bbee080f648c38092d67ee8226cdcb4e
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2022-01-17 00:15:10 +0800

    feat: game

--- game.js
+++ game.js
@@ -18,6 +18,7 @@ function init()
     rePoint();
     fullCharArr();
     genRot13();
+    genRot133();
 }
 function fullCharArr()
 {
@@ -44,6 +45,7 @@ function getRandomInt(max)
 var rot13_q = "";
 function genRot13()
 {
+    
     for(var i = 0 ; i<26 ;i++)
     {
         let x = getRandomInt(50);
@@ -61,6 +63,28 @@ function genRot13()
 
 
     document.getElementById("rot13_str").innerHTML = rot13_q;
+}
+var rot13_qq = "";
+function genRot133()
+{
+    
+    for(var i = 0 ; i<26 ;i++)
+    {
+        let x = getRandomInt(50);
+        let y = getRandomInt(50);
+        let temp = 0;
+        
+        temp = charArr[x];
+        charArr[x] = charArr[y];
+        charArr[y] = temp;
+ 
+    }
+    
+    for(var i = 0 ; i<5 ;i++)
+        rot13_qq += charArr[i];
+
+
+    document.getElementById("rot13_str_11").innerHTML = rot13_qq;
 }
 function qq()
 {
@@ -85,6 +109,18 @@ function rot13()
     let ref = document.getElementById("fb1");
     let button = document.getElementById("b1");
     (document.getElementById("rot13_output").value == rot13_q.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);})) ? (ref.innerHTML="O") : (ref.innerHTML="X") ;
+    if((ref.innerHTML=="O"))
+    {
+        button.style.display = "none";
+        pionts += 10;
+        rePoint();
+    }
+}
+function rot13_11()
+{
+    let ref = document.getElementById("fb11");
+    let button = document.getElementById("b11");
+    (document.getElementById("rot13_output_11").value == rot13_qq.replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);})) ? (ref.innerHTML="O") : (ref.innerHTML="X") ;
     if((ref.innerHTML=="O"))
     {
         button.style.display = "none";
